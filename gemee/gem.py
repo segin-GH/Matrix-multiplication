30ac8a3165701c75e2c172fb3ef740a26f77b9bb
segin-GH <segin.bytes@gmail.com>
2023-02-21 23:04:23 +0530

    .

--- gemee/gem.py
+++ gemee/gem.py
@@ -1,3 +1,19 @@
 #! usr/bin/env python3
+import numpy as np
 
 print("Hello World")
+
+N = 2096
+
+if __name__ == "__main__":
+    # N^2
+    A = np.random.randn(N, N).astype(np.float32)
+    # N^2
+    B = np.random.randn(N, N).astype(np.float32)
+    
+    # N^2 output cells with 2N compute each
+    # flops print(N*N*2*N)
+    
+    print(N*N*2*N)
+
+    C = A @ B
